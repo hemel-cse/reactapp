c57046aebc435a6b4c1d5c210a28d283b873af15
Hemel <hasibul.amin.hemel@gmail.com>
2017-08-04 18:51:03 +0600

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import {
     NavLink
 } from 'react-router-dom';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 
 class Footer extends Component {
     render() {
         return (
-            <nav className="navbar-bottom">
-                <div className="menu-list">
-                    <ul>
-                        <li>
-                            <NavLink to='/contact'>Contact</NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            <div >
+                <nav className="navbar-bottom">
+                    <div className="menu-list">
+                        <ul>
+                            <li>
+                                <NavLink to='/contact'>Contact</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         );
     }
 }
